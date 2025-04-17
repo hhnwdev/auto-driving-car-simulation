65f3e4804758158967606a7e58c2c9cca4c2ee67
Hein Htet Nyunt Win <hhnwdev@gmail.com>
2025-04-17 16:55:52 +0800

    feat: define the simulation class to simulate the car in the car_simulation.py

--- car_simulation.py
+++ car_simulation.py
@@ -45,3 +45,49 @@ class Car:
 
     def __str__(self):
         return f"{self.name}, ({self.x},{self.y}) {self.direction}"
+
+class Simulation:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.cars = []
+
+    def add_car(self, name, x, y, direction, commands):
+        self.cars.append(Car(name, x, y, direction, commands))
+
+    def simulate(self):
+        car_count = len(self.cars)
+        collisions = {}
+        max_steps = max(len(car.commands) for car in self.cars)
+
+        for step in range(max_steps):
+            positions = {}
+            for car in self.cars:
+                if not car.active or step >= len(car.commands):
+                    continue
+                prev_position = car.get_position()
+                car.execute_command(car.commands[step], self.width, self.height)
+                car.step = step + 1
+                pos = car.get_position()
+                if pos in positions:
+                    other = positions[pos]
+                    collisions[car.name] = (pos, step + 1, other.name)
+                    collisions[other.name] = (pos, step + 1, car.name)
+                    car.active = False
+                    other.active = False
+                else:
+                    positions[pos] = car
+        return collisions
+
+    def get_car_info(self):
+        return [f"- {car.name}, ({car.x},{car.y}) {car.direction}, {''.join(car.commands)}" for car in self.cars]
+
+    def get_results(self, collisions):
+        results = []
+        for car in self.cars:
+            if car.name in collisions:
+                pos, step, other = collisions[car.name]
+                results.append(f"- {car.name}, collides with {other} at {pos} at step {step}")
+            else:
+                results.append(f"- {car.name}, ({car.x},{car.y}) {car.direction}")
+        return results
